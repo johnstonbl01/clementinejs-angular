cc3cc2aa295c1bd3227c3834254982265ffb472d
Blake Johnston <johnstonbl01@gmail.com>
2015-05-25 15:55:45 -0500

    Added code comments to all the files

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -1,26 +1,45 @@
 'use strict';
 
+// Define the ClickHandler function object
 function ClickHandler (db) {
+	// Define the database collection to use
+	// A collection is analagous to a SQL table
 	var clicks = db.collection('clicks');
 
+	// Define a getClicks() method
 	this.getClicks = function (req, res) {
+		// Query the database
 		clicks
-			.findOne(
-				{},
-				{ '_id': false },
+			.findOne( // findOne will return a single document
+				{}, // Query parameters - this will return all documents
+				{ '_id': false }, // Projection criteria - this will exclude the _id field from the results
+				// Callback function that executes once the query has finished
 				function (err, result) {
+					// If there is an error, throw an error
 					if (err) { throw err; }
+
+					// Define an empty result array
+					// This is done because the Angular $resource.query needs and array in the API
 					var clickResults = [];
 
+					// If query is successful and returns a result...
 					if (result) {
-
+						// Push the result to the clickResults array
 						clickResults.push(result);
+						// Send the results in JSON format back to the client
 						res.json(clickResults);
+
+					// If query does not return a result...
 					} else {
+
+						// Insert a new document into the database
+						// This traditionally only happens the first time the application is run
 						clicks.insert({ 'clicks': 0 }, function (error, newdoc) {
 							if (err) { throw err; }
 
+							// Push the result of the insertion to the clickResults array
 							clickResults.push(newdoc);
+							// Send the results in JSON format back to the client
 							res.json(clickResults);
 						});
 
@@ -29,28 +48,36 @@ function ClickHandler (db) {
 			);
 	};
 
+	// Define an addClick() method
 	this.addClick = function (req, res) {
 		clicks
-			.findAndModify(
+			.findAndModify( // findAndModify will find a document and modify it
 				{}, // Query parameters -- this will return all documents
 				{ '_id': 1 }, // Sort parameters
 				{ $inc: { 'clicks': 1 } }, // Update parameters -- increment the value of clicks by 1
+				// Callback function that executes once the query has finished
 				function (err, result) {
+					// If there is an error, throw an error
 					if (err) { throw err; }
 
+					// Respond with the result of the query in JSON format
 					res.json(result);
 				}
 			);
 	};
 
+	// Define a resetClicks() method
 	this.resetClicks = function (req, res) {
 		clicks
-			.update(
-				{},
-				{ 'clicks': 0 },
+			.update( // Will update the document found by the query
+				{}, // Query parameters -- this will return all documents
+				{ 'clicks': 0 }, // Updates the document to reset number of clicks
+				// Callback funciton that executes once the query has finished
 				function (err, result) {
+					// If there is an error, throw an error
 					if (err) { throw err; }
 
+					// Respond with the result of the query in JSON format
 					res.json(result);
 				}
 			);
--- server.js
+++ server.js
@@ -8,14 +8,22 @@ var express = require('express'), // Express framework
 // Initialize Express
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clemjsdb', function (err, db) {
+// Connect to Mongo database (clementinejs)
+// If the database doesn't exist, Mongo should dynamically create it
+mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+
+	// If an error occurs while trying to connect to the database...
 	if (err) {
+		// throw a custom error message
 		throw new Error('Database failed to connect!');
+	// If the connection is successfull...
 	} else {
+		// log a success message
 		console.log('MongoDB successfully connected on port 27017.');
 	}
 
 	// Set static folder values for concise folder locations
+	// These allow for easier links in HTML & JS files
 	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 	app.use('/public', express.static(process.cwd() + '/public'));
 
